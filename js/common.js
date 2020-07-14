f772cc3a56246dbea5f988664e342c2d24eb193b
CHHuy <chehoanghuy2015@gmail.com>
2020-07-14 15:31:54 +0700

    Making skeleton - Let's get started

---
+++ js/common.js
@@ -0,0 +1,111 @@
+/*!--------------------------------------------------------------------------------
+
+ Theme Name: Frontend Seed 4
+ Author:     trungnghia112 <trungnghia112@gmail.com>
+
+ -----------------------------------------------------------------------------------*/
+
+if (Modernizr.touch === true && $(window).width() <= 767) {
+  //alert('Touch Screen');
+} else {
+
+}
+
+(function ($) {
+  'use strict';
+
+
+  /* ==================================================
+  # Get scroll bar width
+  ===================================================*/
+  function getBarwidth() {
+    // Create the measurement node
+    let scrollDiv = document.createElement('div');
+    scrollDiv.className = 'scrollbar-measure';
+    document.body.appendChild(scrollDiv);
+
+    // Get the scrollbar width
+    let scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+    //console.warn(scrollbarWidth); // Mac:  15
+
+    // Delete the DIV
+    document.body.removeChild(scrollDiv);
+    return scrollbarWidth;
+  }
+
+  /* ==================================================
+  # Smooth Scroll
+  ===================================================*/
+  function scrollToAnchor() {
+    $('.js-scroll-to').on('click', function (event) {
+      let $anchor = $(this);
+      let headerH = '0';
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr('href')).offset().top - headerH + 'px'
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  }
+
+  function init() {
+    scrollToAnchor();
+    getBarwidth();
+  }
+
+  $(document).ready(function () {
+    init();
+  }); // end document ready function
+
+  $(window).on('scroll', function () {
+  });
+
+  $(document).ready(function(){
+    $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
+
+  // if ($('.x-toTop').length) {
+  //   let scrollTrigger = 100, // px
+  //     backToTop = function () {
+  //       let scrollTop = $(window).scrollTop();
+  //       if (scrollTop > scrollTrigger) {
+  //         $('.x-toTop').addClass('active');
+  //       } else {
+  //         $('.x-toTop').removeClass('active');
+  //       }
+  //     };
+  //   backToTop();
+  //   $(window).on('scroll', function () {
+  //     backToTop();
+  //   });
+  // }
+
+})(jQuery); // End jQuery
